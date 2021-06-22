67ef2e7bf8fcf32bb79c906655db1079df31294e
Narges <ghasemi.narges2000@gmail.com>
2021-06-22 23:08:44 +0430

    proj

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -1,4 +1,4 @@
-"""Project URL Configuration
+"""djangoProject1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,8 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.authtoken import views
+from  restaurant.views import  RegisterUser
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('restaurant/', include('restaurant.urls')),
+    path('customer/', include('customer.urls')),
+    path('api-token-auth/', views.obtain_auth_token),
+    path('register/',RegisterUser.as_view()),
 ]
