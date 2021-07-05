2576b7e0c8ea19ff2e4dec2204d1994eca633544
Narges <ghasemi.narges2000@gmail.com>
2021-07-06 00:06:07 +0430

    Backend

--- Project/djangoProject1/settings.py
+++ Project/djangoProject1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8ovh_r+yyrg2qy=nsbl8-9x$p7%yt_)kwq%421cl&!7jv6x(7q'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
